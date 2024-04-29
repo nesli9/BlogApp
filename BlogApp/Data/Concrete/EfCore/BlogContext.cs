cfccfd00a11052cdcbaa6990d0c697aa9973bac8
Neslihan Doğan <74837454+nesli9@users.noreply.github.com>
2024-04-29 09:57:34 +0300

    Connection String

--- BlogApp/Data/Concrete/EfCore/BlogContext.cs
+++ BlogApp/Data/Concrete/EfCore/BlogContext.cs
@@ -5,7 +5,7 @@ namespace BlogApp.Data.Concrete.EfCore{
     public class BlogContext:DbContext
     {
         public BlogContext(DbContextOptions<BlogContext> options):base(options){
-
+            
         }
         public DbSet<Post> Posts => Set<Post>();
         public DbSet<Comment> Comments => Set<Comment>();
