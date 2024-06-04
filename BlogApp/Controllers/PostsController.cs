823a049c3ba351ace401c44fc39dbbba067d257d
Neslihan Doğan <74837454+nesli9@users.noreply.github.com>
2024-06-04 10:01:47 +0300

    View Components

--- BlogApp/Controllers/PostsController.cs
+++ BlogApp/Controllers/PostsController.cs
@@ -9,15 +9,13 @@ namespace BlogApp.Controllers{
         private IPostRepository _postRepository;
         private ITagRepository _tagRepository;
 
-        public PostsController(IPostRepository postRepository , ITagRepository tagRepository){
+        public PostsController(IPostRepository postRepository ){
             _postRepository = postRepository; //inject yöntemiyle nesne oluşturulması sağlanır
-            _tagRepository = tagRepository;
         }
         public IActionResult Index(){
             return View(
                 new PostsViewModel{
-                    Posts = _postRepository.Posts.ToList(),
-                    Tags = _tagRepository.Tags.ToList()
+                    Posts = _postRepository.Posts.ToList()
                 }
             );
         }
--- BlogApp/Models/PostsViewModel.cs
+++ BlogApp/Models/PostsViewModel.cs
@@ -4,7 +4,6 @@ namespace BlogApp.Models
 {
     public class PostsViewModel{
         public List<Post> Posts { get; set; } = new();
-        public List<Tag> Tags { get; set; } = new();
 
     }
 }
